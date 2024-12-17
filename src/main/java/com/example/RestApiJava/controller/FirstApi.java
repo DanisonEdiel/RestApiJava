4324bce94163b0d4868d994e17782347aa857c3a
Edison Llano <edison.shano@hotmail.com>
2024-12-17 15:08:15 -0500

    add github workflows

--- src/main/java/com/example/RestApiJava/controller/FirstApi.java
+++ src/main/java/com/example/RestApiJava/controller/FirstApi.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class FirstApi {
     @GetMapping("/hello")
     public String sayHello() {
-        return "¡Hola guasa!";
+        return "¡Hola con todos!";
     }
 }
